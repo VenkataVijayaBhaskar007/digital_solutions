e9cf68085bd944b719c492eb78b1015914c92d07
VenkataVijayaBhaskar007 <venkatavijayabhaskarvadada007@gmail.com>
2023-12-31 08:13:27 +0530

    new

--- components/About/About1.tsx
+++ components/About/About1.tsx
@@ -152,6 +152,17 @@ const About1 = () => {
             </div>
             <div className="w-full px-4 lg:w-6/12">
               <div className="flex items-center -mx-3 sm:-mx-4">
+                           
+              <video
+  className="object-contain rounded-3xl w-full"
+  height="600"
+  width="778"
+  src="./images/about/about3.webm"
+  autoPlay
+  muted
+  loop
+/>
+    
               <div className="w-full px-3 sm:px-4 xl:w-1/2">
                   <div className="relative z-10 my-4">
                    
